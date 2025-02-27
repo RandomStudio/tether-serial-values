3b7d9fdc40a455d30f56ab854ef627877cffddb9
Stephen Buchanan <webtvmaster@gmail.com>
2025-02-27 13:07:28 +0100

    v0.2.0 has optional timeout between messages

--- src/bin/tether-serial.rs
+++ src/bin/tether-serial.rs
@@ -4,6 +4,7 @@ use log::*;
 use std::io::BufRead;
 use std::io::BufReader;
 use std::time::Duration;
+use std::time::SystemTime;
 
 use clap::Parser;
 use tether_agent::{PlugOptionsBuilder, TetherAgentOptionsBuilder};
@@ -20,8 +21,22 @@ struct Cli {
     #[arg(long = "baudRate", default_value_t = 9600)]
     baud_rate: u32,
 
+    /// Role to use in Tether topics
     #[arg(long = "tether.role", default_value_t = String::from("serial"))]
     tether_role: String,
+
+    /// ID to use in Tether topics
+    #[arg(long = "tether.id", default_value_t = String::from("any"))]
+    tether_id: String,
+
+    /// Plug Name to use in Tether topics
+    #[arg(long = "tether.plugName", default_value_t = String::from("values"))]
+    tether_plug_name: String,
+
+    /// How long to wait (ms) for first message to be relayed, or next message.
+    /// Program will panic if this timeout is reached. Leave blank to disable the check.
+    #[arg(long = "timeout")]
+    timeout_ms: Option<usize>,
 }
 
 fn main() {
@@ -34,16 +49,19 @@ fn main() {
         .init();
 
     let mut tether_agent = TetherAgentOptionsBuilder::new(&cli.tether_role)
+        .id(Some(&cli.tether_id))
         .build()
         .expect("failed to init Tether Agent");
 
-    let output_plug = PlugOptionsBuilder::create_output("values")
+    let output_plug = PlugOptionsBuilder::create_output(&cli.tether_plug_name)
         .build(&mut tether_agent)
         .unwrap();
 
     let port = serialport::new(&cli.port, cli.baud_rate)
         .timeout(Duration::from_millis(100))
         .open();
+
+    let mut last_value_parsed = SystemTime::now();
 
     match port {
         Ok(mut serial_port) => {
@@ -53,9 +71,18 @@ fn main() {
                 &cli.port, cli.baud_rate
             );
             loop {
+                if let Some(timeout) = cli.timeout_ms {
+                    let elapsed = last_value_parsed.elapsed().unwrap();
+                    if elapsed > Duration::from_millis(timeout as u64) {
+                        error!("Elapsed {}ms > timeout {}ms", elapsed.as_millis(), timeout);
+                        panic!("Reached timeout since start/previous message");
+                    }
+                }
+
                 let mut reader = BufReader::new(&mut serial_port);
                 let mut value = String::new();
                 if reader.read_line(&mut value).is_ok() {
+                    last_value_parsed = SystemTime::now();
                     let s = value.trim();
                     debug!("String: {}", s);
                     if let Ok(int_value) = s.parse::<u32>() {
@@ -71,7 +98,7 @@ fn main() {
             }
         }
         Err(e) => {
-            eprintln!("Failed to open \"{}\". Error: {}", &cli.port, e);
+            error!("Failed to open \"{}\". Error: {}", &cli.port, e);
             panic!("Failed to open port");
         }
     }
